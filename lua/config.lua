49d90e70ab70732ea4d7586f131b531de1a28491
aslink87 <adam.slinkman@gmail.com>
2022-06-06 16:07:32 -0400

    switched to EcoVim profile

---
+++ lua/config.lua
@@ -0,0 +1,41 @@
+------------------------------------------------
+--                                            --
+--    This is a main configuation file for    --
+--                    EcoVim                  --
+--      Change variables which you need to    --
+--                                            --
+------------------------------------------------
+
+local icons = require('icons')
+
+EcoVim = {
+  colorscheme = 'solarized',
+  ui = {
+    float = {
+      border = 'rounded'
+    }
+  },
+  plugins = {
+    completion = {
+      select_first_on_enter = false
+    },
+    dashboard = {
+      fuzzy_plugin = 'telescope' -- telescope/clap/fzf
+    },
+    rooter = {
+      -- Removing package.json from list in Monorepo Frontend Project can be helpful
+      -- By that your live_grep will work related to whole project, not specific package
+      patterns = {'.git', 'package.json', '_darcs', '.bzr', '.svn', 'Makefile'} -- Default
+    },
+    zen = {
+      enabled = false,
+      kitty_enabled = false
+    },
+  },
+  icons = icons,
+  statusline = {
+    path_enabled = true,
+    path = 'relative' -- absolute/relative
+  }
+}
+
