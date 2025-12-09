d0d4bcfc732262fd766b837a8dcdb23d983cd129
aslink87 <adam.slinkman@gmail.com>
2025-12-09 01:42:29 -0500

    still working on folds, still don't work :(

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- -- Only way i am able to get treesitter to fold is in manual mode
-local opt = vim.opt
-opt.foldmethod = "manual"
+-- -- use to use manual, now it's broken afer updates
+-- local opt = vim.opt
+-- opt.foldmethod = "expr"
+-- opt.foldexpr = "v:lua.LazyVim.treesitter.foldexpr()"
+-- opt.foldlevel = 99
+-- opt.foldtext = ""
